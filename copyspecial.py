6b71bd1698b8fb4d060119f25ffe94dc99fcfdcf
knegley <kpnegley@gmail.com>
2020-06-09 10:46:40 -0400

    just adding junk testing code and ideas for part b on copy special

--- copyspecial.py
+++ copyspecial.py
@@ -19,13 +19,86 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    # your code here
-    return
+    if os.path.exists(dirname) and os.path.isdir(dirname):
+        with os.scandir(dirname) as directory:
+            files = [os.path.abspath(item)
+                     for item in directory if item.is_file()]
+            # for item in directory:
+            #     # print(item)
+            #     # print(item.is_file())
+            #     if item.is_file():
+            #         print(os.path.abspath(item))
+            # print("\n".join(files))
+
+        return "\n".join(files)
+    else:
+        print("Path and Directory does not exist")
+
+
+# print(os.getcwd())
+# print(get_special_paths(os.getcwd()))
+# (get_special_paths("helloasdfage"))
+# print(os.listdir(os.getcwd()))
+# with os.scandir(os.getcwd()) as test_direcotry:
+#     for item in test_direcotry:
+#         print(item)
+
+
+# for dir_path, dirname, filename in os.walk(os.getcwd()):
+#     if dirname:
+#         print(f"dirname: {dirname}")
+#     if "copyspecial.py" in filename:
+#         print("copy file is here")
+
+# print(filename)
 
 
 def copy_to(path_list, dest_dir):
     # your code here
+
+    # if os.path.isdir(dest_dir):
+    #     for item in path_list:
+
+    #         if os.path.isfile(item):
+
+    #             for _, _, file_name in os.walk(dest_dir):
+
+    #                 if item in os.path.abspath(file_name):
+    #                     print("file already exists in the dest_dir")
+    #                     return
+    #                 else:
+    #                     shutil.copy(item, dest_dir)
+
+    #             else:
+    #                 print(
+    #                     f"The path: {item} can't be copied because file doesn't exits")
+    #             return
+    # print(f"the directory {dest_dir} does not exist")
+    # dir_response = input(
+    #     "would you like to create a directory with the path_list? respond Y or N\n")
+    # if dir_response == "N":
+    #     return
+    # if dir_response == "Y":
+    # os.mkdir(dest_dir)
+    # destination = os.path.abspath(dest_dir)
+    # print(destination)
+    # print(os.path.exists(destination))
+    for item in path_list:
+        # shutil.copytree(os.path.abspath("xyz_hello_.txt"), destination)
+        # print(os.path.abspath(item))
+        # print(os.path.realpath(item))
+        # print(os.path.basename(item))
+        # final_destination = os.path.join(os.path.basename(item))
+        #     shutil.copy2(os.path.basename(item),
+        #                  os.path.relpath(os.mkdir(dest_dir)))
+        curr_dir = os.getcwd()
+        destination = os.mkdir(dest_dir)
+        source_path = os.path.join(curr_dir, item)
+        shutil.copy2(os.path.abspath(item), destination)
     return
+
+
+copy_to(["xyz_hello_.txt"], "something_cool")
 
 
 def zip_to(path_list, dest_zip):
