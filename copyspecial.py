3cb3ac031dd4db9e764d213d10a3c29d561b780c
Kyle Negley <kpnegley@gmail.com>
2020-06-09 15:41:49 -0400

    help from coach

--- copyspecial.py
+++ copyspecial.py
@@ -19,22 +19,30 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    if os.path.exists(dirname) and os.path.isdir(dirname):
-        with os.scandir(dirname) as directory:
-            files = [os.path.abspath(item)
-                     for item in directory if item.is_file()]
-            # for item in directory:
-            #     # print(item)
-            #     # print(item.is_file())
-            #     if item.is_file():
-            #         print(os.path.abspath(item))
-            # print("\n".join(files))
+    # if os.path.exists(dirname) and os.path.isdir(dirname):
+    #     with os.scandir(dirname) as directory:
+    #         files = [os.path.abspath(item)
+    #                  for item in directory if item.is_file()]
+    #         # for item in directory:
+    #         #     # print(item)
+    #         #     # print(item.is_file())
+    #         #     if item.is_file():
+    #         #         print(os.path.abspath(item))
+    #         # print("\n".join(files))
 
-        return "\n".join(files)
-    else:
-        print("Path and Directory does not exist")
+    #     return "\n".join(files)
+    # else:
+    #     print("Path and Directory does not exist")
 
+    special_files = []
+    list_paths = os.listdir(dirname)
+    for file in list_paths:
+        pattern = re.search(r"[__](\w+)[__]", file)
+        if pattern:
+            special_files.append(os.path.abspath(
+                (os.path.join(dirname, file))))
 
+    return special_files
 # print(os.getcwd())
 # print(get_special_paths(os.getcwd()))
 # (get_special_paths("helloasdfage"))
@@ -54,6 +62,15 @@ def get_special_paths(dirname):
 
 
 def copy_to(path_list, dest_dir):
+    if not os.path.isdir(dest_dir):
+
+        os.makedirs(dest_dir)
+        # shutil.copy(os.path.abspath(path_list), os.path.abspath(directory))
+
+    for item in path_list:
+        path = os.path.join(dest_dir, os.path.basename(item))
+        shutil.copy(item, path)
+
     # your code here
 
     # if os.path.isdir(dest_dir):
@@ -83,40 +100,52 @@ def copy_to(path_list, dest_dir):
     # destination = os.path.abspath(dest_dir)
     # print(destination)
     # print(os.path.exists(destination))
-    for item in path_list:
-        # shutil.copytree(os.path.abspath("xyz_hello_.txt"), destination)
-        # print(os.path.abspath(item))
-        # print(os.path.realpath(item))
-        # print(os.path.basename(item))
-        # final_destination = os.path.join(os.path.basename(item))
-        #     shutil.copy2(os.path.basename(item),
-        #                  os.path.relpath(os.mkdir(dest_dir)))
-        curr_dir = os.getcwd()
-        destination = os.mkdir(dest_dir)
-        source_path = os.path.join(curr_dir, item)
-        shutil.copy2(os.path.abspath(item), destination)
-    return
+    # for item in path_list:
+    # shutil.copytree(os.path.abspath("xyz_hello_.txt"), destination)
+    # print(os.path.abspath(item))
+    # print(os.path.realpath(item))
+    # print(os.path.basename(item))
+    # final_destination = os.path.join(os.path.basename(item))
+    #     shutil.copy2(os.path.basename(item),
+    #                  os.path.relpath(os.mkdir(dest_dir)))
+    # curr_dir = os.getcwd()
+    # destination = os.mkdir(dest_dir)
+    # source_path = os.path.join(curr_dir, item)
+    # shutil.copy2(os.path.abspath(item), destination)
 
-
-copy_to(["xyz_hello_.txt"], "something_cool")
+# copy_to(["xyz_hello_.txt"], "something_cool")
 
 
 def zip_to(path_list, dest_zip):
-    # your code here
+
     return
 
 
-def main(args):
+def main():
     """Main driver code for copyspecial."""
     # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
     # TODO: add one more argument definition to parse the 'from_dir' argument
-    ns = parser.parse_args(args)
-
+    parser.add_argument("from_dir", help="return directory")
+    args = parser.parse_args()
     # TODO: you must write your own code to get the command line args.
     # Read the docs and examples for the argparse module about how to do this.
+
+    # listing out args from namespace from the parser
+    from_dir = args.from_dir
+    copy_to_dir = args.todir
+    zip_dir = args.tozip
+
+    special_paths = get_special_paths(from_dir)
+
+    if copy_to_dir:
+        copy_to(special_paths, copy_to_dir)
+    elif zip_dir:
+        zip_to(special_paths, zip_dir)
+    else:
+        print("\n".join(special_paths))
 
     # Parsing command line arguments is a must-have skill.
     # This is input data validation. If something is wrong (or missing) with
@@ -127,4 +156,4 @@ def main(args):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
